4deb8ba0492efaccdb1f0b711be3f4636b83e98e
Mario Lopez <ez0212sp@icloud.com>
2021-01-14 21:55:11 -0500

    commit tree 379c73dcf2e1ac45a2396b5437063b85d4bf3386

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    def initialize (anagram)
+        @anagram = anagram
+    end
+    
+    def match(words)
+        words.select do |word|
+          is_anagram?(word)
+        end
+      end
+    
+      def is_anagram?(word)
+        word.chars.sort == @anagram.chars.sort
+      end
+
+  
+end 
+
